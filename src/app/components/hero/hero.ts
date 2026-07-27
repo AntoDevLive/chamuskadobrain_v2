2257c27a05650fbf2acf203ce3d88216057756ff
antoniocp99 <antoniocastizo99@gmail.com>
2026-07-27 17:27:18 +0200

    servicios-page traducida

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-hero',
-  imports: [TranslocoPipe],
+  imports: [TranslocoPipe, RouterLink],
   templateUrl: './hero.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/pages/servicios-page/servicios-page.ts
+++ src/app/pages/servicios-page/servicios-page.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IServicio } from '../../interfaces/servicio';
 import { serviciosJson } from '../../json/servicios';
 import { RouterLink } from "@angular/router";
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-servicios-page',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslocoPipe],
   templateUrl: './servicios-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
